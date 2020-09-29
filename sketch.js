17d08f5e065dd697211250db10c88cfcede5b6e9
trapuntino <71377169+trapuntino@users.noreply.github.com>
2020-09-29 22:32:29 +0200

    Assignment

--- sketch.js
+++ sketch.js
@@ -3,10 +3,16 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+createCanvas(750,750);
+background("black");
+frameRate(12);
   // put setup code here
 }
 
 function draw() {
+noStroke();
+fill(lerpColor(color("#3d348b"), color("#c71f37"),
+frameCount/120));
+rect(mouseX, mouseY, 60, 100, 40);
   // put drawing code here
 }
